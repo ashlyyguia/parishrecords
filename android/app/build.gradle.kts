63127b08d686c32467569da5ae66204c9516761e
johncris09 <johncrismanabo9@gmail.com>
2026-06-30 14:15:52 +0800

    fix: Update NDK version and Java home path in build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.parishrecord"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.1.12297006"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
